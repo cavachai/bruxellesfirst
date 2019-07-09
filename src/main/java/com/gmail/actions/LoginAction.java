57ed8f90938564ef13991b4130f72fae0b949784
Roman Bodak <roman_bodak@epam.com>
2019-07-09 16:27:49 +0300

    wip: debug

--- src/main/java/com/gmail/actions/LoginAction.java
+++ src/main/java/com/gmail/actions/LoginAction.java
@@ -1,10 +1,14 @@
 package com.gmail.actions;
 
+import com.driver.WebDriverContainer;
 import com.gmail.pages.LoginPage;
 import com.google.inject.Inject;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.FluentWait;
+
+import java.beans.Visibility;
 
 public class LoginAction {
 
@@ -17,6 +21,7 @@ public class LoginAction {
 
     public void typeEmail() {
         loginPage.getEmailInput().sendKeys("epamtestmail2@gmail.com", Keys.ENTER);
+        FluentWait<WebDriver> elementFluentWait = new FluentWait<WebDriver>(WebDriverContainer.getDriver()).until(e -> !loginPage.getEmailInput().isDisplayed());
 //        loginPage.getEmailNextButton().click();
     }
 
